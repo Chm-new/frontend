269c6e154e39217eb7c733a630afce799db67bdc
Chm-new <jerry40119475@gmail.com>
2025-01-13 17:05:45 +0900

    delet + modify

--- src/components/BoardDetail.js
+++ src/components/BoardDetail.js
@@ -1,49 +1,69 @@
-import React,{useEffect,useState } from 'react'
-import { useLocation,useParams } from 'react-router';
-import axios from 'axios';
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router'
 import './BoardStyle/BoardDetail.css';
 
 const BoardDetail = () => {
-    const [data,setData] = useState(null)
-    const location = useLocation();
-    const pathVariable = location.pathname.split("/")   // path(경로)이름에서 /를 기준으로 나누어 배열로 반환   
-    const id = pathVariable[2]  // 배열의 2번째 인덱스 값이 id값이다.
-    console.log(id)
-
-   useEffect(() => {
-
-    const requstFunc = async () => {
-        console.log("여기는 전체 목록 조회")
-        const res = await axios.get(`http://localhost:8080/api/board/detail/${id}`)
+  const [data,setData] =useState(null)
+   const location =useLocation()
+   const pathVariable = location.pathname.split("/")
+   const bno= pathVariable[2]
+   console.log(bno)
+   const [title, setTitle] = useState('')
+   const [content, setContent] = useState('')
+   const [writer, setWriter] = useState('')
+   //데이터 조회 
+  useEffect(()=>{
+    const requestFunc = async ()=>{
+        console.log("여기는 데이터 하나  조회에요 ")
+        const res = await axios.get(`http://localhost:8080/api/board/detail/${bno}`)
         console.log(res)
-        setData(res.data)   
+        setData(res.data)
+        setTitle(res.data.title)
+        setContent(res.data.content)
+        setWriter(res.data.writer)
     }
-    requstFunc()
-   }, [])
+    requestFunc()
+},[bno])
+//데이터 수정 
+   const handleUpdate = async (e)=>{
+    e.preventDefault()//새로 고침 방지 
+    try{
+        const res = await axios.put(`http://localhost:8080/api/board/detail/${bno}`,
+          {title, content ,bno, writer}
+        )
+        console.log(res)
+        alert("게시글이 성공적으로 수정되었습니다")
+        setData(res.data)
+    } catch(error){
 
+    }
+
+}
+   
   return (
     <div>
       {data?<form>
-        
         <div>
-            <label>번호</label>
-            <input name='bno' value={data.bno}/>
+          <label>번호</label>
+          <input  name='bno' value={bno} readOnly/>
         </div>
         <div>
-            <label>제목</label>
-            <input name='title' value={data.title}/>
+          <label>제목</label>
+          <input  name='title' value={title} onChange={(e)=>setTitle(e.target.value)}/>
         </div>
         <div>
-            <label>내용</label>
-            <input nema='content' value={data.content}/>
-        </div>
+          <label>내용</label>
+          <textarea  name='content' onChange={e=>setContent(e.target.value)}
+            rows="10" cols="10">{data.content}</textarea>
+      </div>
         <div>
-            <label>작성자</label>
-            <input nema='title' value={data.writer}/>
+          <label>작성자</label>
+          <input  name='writer' value={writer} readOnly/>
         </div>
-
+        <button onClick={handleUpdate}> 게시글 수정 </button>
       </form>
-      :<p>로딩중...</p>}
+       :<p>Loading ...</p>}
     </div>
   )
 }
--- src/components/BoardList.js
+++ src/components/BoardList.js
@@ -1,46 +1,68 @@
 import React from 'react'
 import axios from "axios";
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './BoardStyle/BoardList.css';
 
 const BoardList = () => {
-    const [data,setData] = useState([])
+    const [data, setData] = useState([])
+    const location = useLocation()
     useEffect(() => {
-        //  async 공부.
         const requstFunc = async () => {
             const res = await axios.get("http://localhost:8080/api/board/list")
             console.log(res)
             setData(res.data)   
         }
         requstFunc()
-
-        
     }, [])
-  
-   
+    const pathVariable = location.pathname.split("/")   
+    const bno = pathVariable[2]
+    console.log(bno) 
+    const deletHandler = async (e, bno) => { 
+        console.log(bno) 
+        await axios.delete(`http://localhost:8080/api/board/delete/${bno}`)
+        window.location.reload()
+        alert("게시글이 삭제되었습니다")
+    }
 
-  return (
-    <div>
-        <table>
-            <h1>구매평 게시판</h1>
-            <tr>
-                <th>번호</th>
-                <th>제목</th>
-                <th>작성자</th>
-                <th>작성일</th>
-            </tr>
-
-            {data.map(i=>(<tr>
-                <td> <Link to = {`/detail/${i.bno}`}> {i.bno} </Link> </td>
-                <td> {i.title} </td>
-                <td> {i.writer} </td>
-                <td> {i.content} </td>
-                </tr>))} 
-
-        </table>
-    </div>
-  )
+    return (
+        <div>
+            <div>
+                <h1>구매평 게시판</h1>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>번호</th>
+                            <th>제목</th>
+                            <th>작성자</th>
+                            <th>내용</th>
+                            <th>삭제</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.map(i => (
+                            <tr key={i.bno}>
+                                <td> <Link to={`/detail/${i.bno}`}> {i.bno} </Link> </td>
+                                <td> {i.title} </td>
+                                <td> {i.writer} </td>
+                                <td> {i.content} </td>
+                                <td> <button onClick={(e) => deletHandler(e, i.bno)}> 삭제 </button> </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <td colSpan="5">
+                                <button className="register">
+                                    <Link to="/register">글쓰기</Link>
+                                </button>
+                            </td>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+        </div>
+    )
 }
 
 export default BoardList
